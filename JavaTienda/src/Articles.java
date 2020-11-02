c2a1c065b694aa4e9217487beb3247674f284113
markes41 <marcosemmanuelsequeira@outlook.com>
2020-11-01 22:32:31 -0300

    Getter & setters y crear/escribir archivo

--- JavaTienda/src/Articles.java
+++ JavaTienda/src/Articles.java
@@ -3,4 +3,39 @@ public class Articles {
 	private int code;
 	private String desc, name;
 	private double price;
+	
+	public Articles(int code, String desc, String name, double price) {
+		super();
+		this.code = code;
+		this.desc = desc;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
--- JavaTienda/src/Main.java
+++ JavaTienda/src/Main.java
@@ -1,20 +1,40 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		File articles = new File("articulos.txt");
+			if(!articles.exists()) {
+				articles.createNewFile();
+			}
 		
-		
-		
-		
-		
-		
-		
-		
+			FileWriter write = new FileWriter(articles);
+			BufferedWriter bw = new BufferedWriter(write);
+			
+			System.out.println("Escribi un texto: ");
+			bw.write(sc.next());
+			bw.close();
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		sc.close();
-
 	}
 
 }
