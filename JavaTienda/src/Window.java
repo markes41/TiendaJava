2a31b6f142f8fc1586c39ed1179c030bec61ac67
markes41 <marcosemmanuelsequeira@outlook.com>
2020-11-10 03:49:21 -0300

    Se agrega el panel

--- JavaTienda/src/Window.java
+++ JavaTienda/src/Window.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
 
-public class Window extends JFrame
-{
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.JPanel;
+
+public class Window extends JFrame{
+	
 	public Window() {
 		super("Stock de artículos");
 		this.setSize(800,600);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null); //CENTRAMOS VENTANA
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //TERMINA EL PROGRAMA AL APRETAR LA X
+		iniciarComponentes();
+	}
+	
+	private void iniciarComponentes() {
+		JPanel panel = new JPanel();
+		this.getContentPane().add(panel);//SE AGREGA EL PANEL
+		
 	}
 }
+
