f9563d29389b73f8bfb09859998581d3f2b38034
markes41 <marcosemmanuelsequeira@outlook.com>
2020-11-11 04:31:41 -0300

    Cambios en la clase Window
    Ahora se toma la información que está en el label y se coloca en el articulos.txt

--- JavaTienda/src/Main.java
+++ JavaTienda/src/Main.java
@@ -8,22 +8,31 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		FileWriter write = null;
+		BufferedWriter bw = null;
 		
-		/*
+		
 		Window ventana = new Window();
 		ventana.setVisible(true); //VENTANA VISIBLE
-		*/
+		
 
 		 File articles = new File("articulos.txt");
 			if(!articles.exists()) {
 				articles.createNewFile();
 			}
-		
-			FileWriter write = new FileWriter(articles);
-			BufferedWriter bw = new BufferedWriter(write);
+			
+			//Seteamos estos 2 parámetros, no se sobreescriba en lo que ya está en el .txt
+			write = new FileWriter(articles.getAbsoluteFile(), true); 
+			bw = new BufferedWriter(write);
 			
 			
-
+			bw.write(ventana.getTexto());
+			
+			
+			if(bw != null)
+				bw.close();
+			if(write != null)
+				bw.close();
 			
 			
 			
--- JavaTienda/src/Window.java
+++ JavaTienda/src/Window.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
-import javax.swing.JRadioButton;
-import java.awt.Color;
-import java.awt.event.*;
 import javax.swing.JPanel;
 
 
 public class Window extends JFrame{
 	JPanel panel;
+	private String texto;
 	
 	public Window() {
 		super("Stock de artículos");
@@ -18,25 +16,39 @@ public class Window extends JFrame{
 	
 	private void iniciarComponentes() {
 		colocarPanel();
-		colocarTexto();
-		//colocarBotones();
-		//colocarChecks();
+		colocarCajaTexto();
+		colocarBotones();	
 	}
 	
 	private void colocarPanel() {
 		panel = new JPanel();
+		panel.setLayout(null); //Agregamos el setLayout en null para que nos tome el setBounds de los botones, labels, etc
 		this.getContentPane().add(panel);//SE AGREGA EL PANEL
 	}
 	
 	private void colocarBotones() {
-		JButton boton1 = new JButton("SALIR");
-		boton1.setBounds(144, 200, 100, 100);
+		JButton boton1 = new JButton("AGREGAR");
+		boton1.setBounds(300, 240, 100, 50);
 		panel.add(boton1);
 	}
 	
-	private void colocarTexto() {
-		JRadioButton check1 = new JRadioButton();
+	private void colocarCajaTexto() {
+		JTextField cajaTexto = new JTextField();
+		cajaTexto.setBounds(300, 200, 200, 30);
+		texto = cajaTexto.getText();
+		setTexto(texto);
+		panel.add(cajaTexto);
 		
 	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
 }
 
