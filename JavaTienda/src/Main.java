383d5f5e44ec11763ad16484547a8f442deb0cc9
Marcio Abriola <marcioabriola@gmail.com>
2020-11-20 03:30:11 -0300

    Update Main.java
    Optimizacion de lineas

--- JavaTienda/src/Main.java
+++ JavaTienda/src/Main.java
@@ -1,7 +1,4 @@
-import java.awt.event.ActionEvent;
-import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) throws IOException{
