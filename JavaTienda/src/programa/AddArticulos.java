0f6f070772be6da43d7bc8a4e5199abdc6c9bdc7
markes41 <marcosemmanuelsequeira@outlook.com>
2020-11-29 01:37:06 -0300

    Editar Articulos funcional 1

--- JavaTienda/src/programa/AddArticulos.java
+++ JavaTienda/src/programa/AddArticulos.java
@@ -8,52 +8,65 @@ import javax.swing.table.DefaultTableModel;
 public class AddArticulos implements Serializable {
 
 	private static final long serialVersionUID = -2737834430344017259L;
-	private HashMap<String, Articulo> diccionarioArticulos = new HashMap<String, Articulo>();
+	private HashMap<Integer, Articulo> diccionarioArticulos = new HashMap<Integer, Articulo>();
 	Articulo res;
 
 	public boolean addArticulos(int codigo, String nombre, double precio, int cantidad) {
 		
-		if(existeArticulo(nombre)){
+		if(existeArticulo(codigo)){
 			return false;
 		}
 		
 		
 		Articulo art = new Articulo(codigo, nombre, precio, cantidad);
 
-		this.diccionarioArticulos.put(nombre.toLowerCase(), art);
+		this.diccionarioArticulos.put(codigo, art);
 		
 		return true;
 	}
 	
-	public boolean existeArticulo(String nombre) {
-		return this.diccionarioArticulos.containsKey(nombre.toLowerCase());
+	public boolean existeArticulo(int codigo) {
+		return this.diccionarioArticulos.containsKey(codigo);
 	}
 	
-	public Articulo getArticulo(String nombre) {
-		return this.diccionarioArticulos.get(nombre.toLowerCase());
+	public Articulo getArticulo(int codigo) {
+		return this.diccionarioArticulos.get(codigo);
 	}
 	
 	public void mostrarArticulos() {
 		
-		for( String clave : this.diccionarioArticulos.keySet()   ) {
-			System.out.println(clave);
-			System.out.println(this.getArticulo(clave));
-			
-			
+		for( int clave : this.diccionarioArticulos.keySet()   ) {
+			System.out.println("Clave del artículo: "+clave+"\n"+this.getArticulo(clave)+"\n");
 		}
 		
 	}
 	
-<<<<<<< HEAD
-	public void getCantidad(String nombre) {
+	public boolean modificarArticulo() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingresa la nueva cantidad: ");
-		int setearCantidad = sc.nextInt();
-		this.diccionarioArticulos.get(nombre.toLowerCase()).setCantidad(setearCantidad);
+		mostrarArticulos();
+		System.out.println("Seleccione el artículo que quiere editar, escribiendo su codigo: ");
+		int codeArticle = sc.nextInt();
+		if(existeArticulo(codeArticle)) {
+		
+			System.out.println("Ingrese el nuevo nombre: ");
+			String nuevoNombre = sc.next();
+			
+			System.out.println("Ingresa la nueva cantidad: ");
+			int nuevaCantidad = sc.nextInt();
+			
+			System.out.println("Ingrese el nuevo precio: ");
+			double nuevoPrecio = sc.nextDouble();
+			
+			Articulo changeArticle = new Articulo(codeArticle, nuevoNombre, nuevoPrecio, nuevaCantidad);
+			diccionarioArticulos.replace(codeArticle, changeArticle);
+			
+			
+			return true;
+		}else {
+			return false;
+		}
+		
 	}
-=======
-
->>>>>>> 81f8fe4ae17132d63dfbf31b8f20db12fb6c63e0
 	
 	
 
